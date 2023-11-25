f23e341ed09ff3d23d4c872a9a478ed476d15980
echandler <chandleric@hotmail.com>
2023-11-24 20:57:17 -0800

    Update testzig8zagCheatIndex.js

--- misc/testzig8zagCheatIndex.js
+++ misc/testzig8zagCheatIndex.js
@@ -1,17 +1,21 @@
 let ___int = setInterval(async ()=>{
         if (!MWGTM_M) return;
+        
+        const obj = {
+            map: MWGTM_M,
+            GM_addStyle: GM_addStyle
+        };
 
         clearInterval(___int);
 
         eval(await fetch('https://echandler.github.io/test-geo-noob-script/misc/usatest.js').then(x => x.text()));
-        usaPlates({
-            map: MWGTM_M,
-            GM_addStyle: GM_addStyle
-        });
+        usaPlates(obj);
 
         eval(await fetch('https://echandler.github.io/test-geo-noob-script/misc/brazilCodesTest.js').then(x => x.text()));
-        brazilCodes({
-            map: MWGTM_M,
-            GM_addStyle: GM_addStyle
-        });
+        brazilCodes(obj);
+
+        eval(await fetch('https://echandler.github.io/Simple-Reverse-Geocoding-Script/reverseGeocodingScript.user.js').then(x => x.text()));
+        
+        eval(await fetch('https://echandler.github.io/test-geo-noob-script/misc/chillStreaks.js').then(x => x.text()));
+        chillStreaks(obj);
 }, 1000);
