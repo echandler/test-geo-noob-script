8a4ccf8b2b74ba2c5277bde62676934f63ebe13f
echandler <chandleric@hotmail.com>
2024-08-14 02:54:49 -0700

    Update test1.js

--- misc/test1.js
+++ misc/test1.js
@@ -471,8 +471,8 @@ setInterval(()=>{
 
         },
         html: `
-            <div class="_rmc_header" >Radom Map Challenge Final Score!</div>
-            <div id="_alert" style="color: green; font-size: 1.2em; margin: 1em 0em;">
+            <div class="_rmc_header"  >Radom Map Challenge Final Score!</div>
+            <div id="_alert" style="style="backgroud-color: #00800030; padding:1em; "color: green; font-size: 1.2em; margin: 1em 0em;">
                 Challenge has ended! Your score is ${ls.maps.length}!
             </div>
             <div>
